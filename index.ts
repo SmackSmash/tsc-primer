38371d36ba5425a06bb7732e136e8ed73b31761d
Dan Moffat <danmoffat@gmail.com>
2024-04-05 15:47:22 +0100

    Made a basic function to fetch data with axios

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-const hello: string = 'Hello World';
+import axios from 'axios';
+
+const fetchTodo = async () => {
+  try {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+    console.log(response.data);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+fetchTodo();
