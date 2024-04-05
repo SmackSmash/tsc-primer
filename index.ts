582415d31269d8ceeb0d33cdd7d0398c98079624
Dan Moffat <danmoffat@gmail.com>
2024-04-05 15:50:56 +0100

    Using ts-node so no index.js required

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ const fetchTodo = async () => {
   try {
     const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
     console.log(response.data);
-  } catch (error) {
+  } catch (error: any) {
     console.error(error.message);
   }
 };
