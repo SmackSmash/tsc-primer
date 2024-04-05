27b59396429ff6ebda6841f5baed137c30b2db71
Dan Moffat <danmoffat@gmail.com>
2024-04-05 16:19:25 +0100

    Some more types

--- index.ts
+++ index.ts
@@ -6,14 +6,21 @@ interface Todo {
   completed: boolean;
 }
 
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`ID: ${id}\nTitle: ${title}\nCompleted: ${completed}`);
+};
+
 (async () => {
   try {
     const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+
     const todo = response.data as Todo;
+
     const id = todo.id;
     const title = todo.title;
     const completed = todo.completed;
-    console.log(`ID: ${id}\nTitle: ${title}\nCompleted: ${completed}`);
+
+    logTodo(id, title, completed);
   } catch (error: any) {
     console.error(error.message);
   }
