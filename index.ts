401f9bbf09e3931c560e81cc22d95fb8dbf83816
Dan Moffat <danmoffat@gmail.com>
2024-04-05 16:02:52 +0100

    Added types to destructured object

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 (async () => {
   try {
     const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
-    const { id, title, completed } = response.data;
+    const { id, title, completed }: { id: number; title: string; completed: boolean } = response.data;
     console.log(`ID: ${id}\nTitle: ${title}\nCompleted: ${completed}`);
   } catch (error: any) {
     console.error(error.message);
