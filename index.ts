417e085610f741023e2d4fb0c6a2ca4abe3ca2dd
Dan Moffat <danmoffat@gmail.com>
2024-04-05 16:13:39 +0100

    Set up my first interface

--- index.ts
+++ index.ts
@@ -1,9 +1,18 @@
 import axios from 'axios';
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 (async () => {
   try {
     const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
-    const { id, title, completed }: { id: number; title: string; completed: boolean } = response.data;
+    const todo = response.data as Todo;
+    const id = todo.id;
+    const title = todo.title;
+    const completed = todo.completed;
     console.log(`ID: ${id}\nTitle: ${title}\nCompleted: ${completed}`);
   } catch (error: any) {
     console.error(error.message);
