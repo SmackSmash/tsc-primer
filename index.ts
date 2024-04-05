c8624b6032657eb9385c67bbb82aca35fdd0fc8d
Dan Moffat <danmoffat@gmail.com>
2024-04-05 19:39:53 +0100

    Moving to windows

---
+++ index.ts
@@ -0,0 +1,14 @@
+// Basics
+let firstName: string = 'Dan';
+let age: number = 37;
+let isDepressed: boolean = true;
+let nothing: null = null;
+let moreNothing: undefined = undefined;
+let now: Date = new Date();
+
+// Arrays
+let arr: any[] = ['red', 5, false];
+let colors: string[] = ['red', 'green', 'blue'];
+let truths: boolean[] = [true, false];
+
+// Classes
