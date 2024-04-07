0a1b1fd8f3e244db42de6c6172905ef329ab9912
Dan Moffat <danmoffat@gmail.com>
2024-04-07 21:19:25 +0100

    Added function declaration

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -1,2 +1,7 @@
 // Always add return type so TS will check your return statement
 const add = (a: number, b: number): number => a + b;
+
+// Works the same way with function declarations
+function divide(a: number, b: number): number {
+  return a / b;
+}
