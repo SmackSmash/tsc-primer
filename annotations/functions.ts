64de3daf6fd5b5676b993a37657c8fcaa124a9ea
Dan Moffat <danmoffat@gmail.com>
2024-04-07 21:56:01 +0100

    Added void and never

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -5,7 +5,18 @@ const add = (a: number, b: number): number => a + b;
 function divide(a: number, b: number): number {
   return a / b;
 }
+
 // and expressions
 const multiply = function (a: number, b: number): number {
   return a * b;
+};
+
+// void for no return value
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+// never for when the function will be exited early
+const throwError = (message: string): never => {
+  throw new Error(message);
 };
