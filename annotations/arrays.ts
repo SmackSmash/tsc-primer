d2d67e00ff57c52264f6153a57ebbb8bd11347e8
Dan Moffat <danmoffat@gmail.com>
2024-04-07 22:54:02 +0100

    More array stuff

--- annotations/arrays.ts
+++ annotations/arrays.ts
@@ -1,2 +1,12 @@
 const manufacturers = ['ford', 'toyota', 'chevy'];
 const dates = [new Date(), new Date()];
+
+// 2 dimensional arrays
+const carsByMake: string[][] = [['f150'], ['corolla'], ['camaro']];
+
+// Help with inferrence when extracting values
+const car = manufacturers[0];
+const myCar = manufacturers.pop();
+
+// Prevent incompatible values
+manufacturers.push(100);
